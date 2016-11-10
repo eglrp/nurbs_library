171919bc28cea1ff516d334b1773a2b6d780019d
miaodi <miaodi1987@gmail.com>
2016-11-10 16:38:52 -0500

    add several functions

--- NurbsCurve.cpp
+++ NurbsCurve.cpp
@@ -3,3 +3,4 @@
 //
 
 #include "NurbsCurve.h"
+
--- main.cpp
+++ main.cpp
@@ -7,11 +7,16 @@ int main() {
     Vector2d point3(5, 5);
     Vector2d point4(3, 2);
 
-    VectorXd knotVector(7);
-    knotVector << 0, 0, 0, .5, 1, 1, 1;
-    Matrix<Matrix<double, 2, 1>, Dynamic, 1> points(4);
-    points << point1, point2, point3, point4;
+    VectorXd knotVector(11);
+    knotVector << 0,0,0,1,2,4,4,5,6,6,6;
+    Matrix<Matrix<double, 2, 1>, Dynamic, 1> points(8);
+    points << point1, point2, point3, point4,point1, point2, point3, point4;
     NurbsCurve<double, 2> a(points, knotVector, 2);
-    cout << a.getDOF();
+
+    cout<<a(2)<<endl;
+    NurbsCurve<double, 2> b(a);
+    cout<<b(2)<<endl;
+    b.degreeElevate(1);
+
     return 0;
 }
